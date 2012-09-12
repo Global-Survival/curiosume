aaf0887b33161bf69bfc67ba11a8b95d5ed86cdd
me <me@me-AO722.(none)>
2012-09-12 13:45:19 -0700

    added chat features via now.js

--- indicator.js
+++ indicator.js
@@ -1,24 +1,28 @@
 /*
-Client-side "Indicator Plugin"
+Indicator Plugin
+
+contains code applicable to both client and server, though each
+won't access the wrong code inadvertently as they are in separate
+methods.
 */
 
 var Indicator = {
     
     id: function() { "" },
     
-    //parent "folder" in the taxonomy, separated by slashes, like "Society/Crime"
-    path: function() { "" },
-    
-    //called when this plugin is activated, to load and parse any necessary remote data (from web server)
-    load: function() { },
-
-    //called when de-activated, in case there is opportunity to free memory using JS "delete" operator
-    unload: function() { },
-    
-    //an array describing variables, their initial values, and limits, to generate UI controls for adjusting those variables
+    //list of named variables, their types, initial values, and limits, to generate UI controls for adjusting those variables
     options: function() { },
     
-    /*adjust result vector for the conditions calculated at a certain geopoint according to hueristics defined by option variables
+    //SERVER to refresh the data in this cache, should be called at least once at the beginning
+    refresh: function(onFinished, onError) { },
+    
+    //CLIENT called when this plugin is activated, to load and parse any necessary remote data (from web server)
+    load: function() { },
+
+    //CLIENT called when de-activated, in case there is opportunity to free memory using JS "delete" operator
+    unload: function() { },
+    
+    /*CLIENT adjust result vector for the conditions calculated at a certain geopoint according to hueristics defined by option variables
     result vector includes any markers or labels to be drawn on the map*/
     update: function(geopoint, result) { }
     
