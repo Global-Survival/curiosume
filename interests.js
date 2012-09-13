c553855aeaf815f36f13832ea384cad2624b3b12
me <me@me-AO722.(none)>
2012-09-13 13:35:39 -0700

    modularizing..

---
+++ interests.js
@@ -0,0 +1,52 @@
+
+var interests = {
+
+
+                            <li>Shelter
+                                <ul>
+                                    <li>Bridges</li>
+                                    <li>Homeless Shelters</li>
+                                    <li>Hotels</li>
+                                    <li>Campgrounds</li>
+                                    <li>CouchSurfing</li>
+                                    <li>Hostels</li>
+                                    <li>Apartments and Sublets</li>
+                                    <li>Real Estate</li>
+                                    <li>Occupy</li>
+                                </ul>
+                            </li>
+                            <li>Food
+                                <ul>
+                                    <li>Groceries<br/>
+                                        <input type="range"  min="0" max="100" />
+                                    </li>
+                                    <li>Edible Vegetation</li>
+                                    <li>Public Kitchens and Potlucks</li>
+                                    <li>Community Gardens</li>
+                                </ul>
+                            </li>
+                            <li>Supplies and Tools</li>
+                            <li>Healthcare</li>
+                            <li>Weather, Climate, & Geology
+                                <ul>
+                                    <li>USGS Earthquakes</li>
+                                </ul>
+                            </li>
+                            <li>Pollution
+                                <ul>
+                                    <li>Nuclear Facilities<br/>
+                                        <input type="range"  min="0" max="100" />
+                                    </li>
+                                    <li>Superfund Sites</li>
+                                </ul>
+                            </li>
+                            <li>Society & Community</li>
+                            <li>Transportation</li>
+                            <li>Communication</li>
+                            <li>Money</li>
+                            <li>Entertainment</li>
+                            <li>Freedom</li>
+
+
+};
+
