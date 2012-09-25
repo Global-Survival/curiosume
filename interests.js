73997133e0b9505f6bfa45639d7b068a003410c2
me <me@me-AO722.(none)>
2012-09-25 12:59:32 -0400

    updates

--- interests.js
+++ interests.js
@@ -1,52 +1,64 @@
 
-var interests = {
+var interestsTree = [
+	'Shelter': [
+		'Bridges': { },
+        	'Homeless Shelters': { },
+            	'Hotels': { },
+            	'Campgrounds': { },
+            	'CouchSurfing': { },
+            	'Hostels': { },
+            	'Apartments and Sublets': { },
+            	'Real Estate': { },
+            	'Occupy': { }
+	],
+	'Food': [ ]
+];
+/*
+
+    'Shelter
+        <ul>
+            'Bridges': { }
+            'Homeless Shelters': { }
+            'Hotels': { }
+            'Campgrounds': { }
+            'CouchSurfing': { }
+            'Hostels': { }
+            'Apartments and Sublets': { }
+            'Real Estate': { }
+            'Occupy': { }
+        </ul>
+    ': { }
+    'Food
+        <ul>
+            'Groceries<br/>
+                <input type="range"  min="0" max="100" />
+            ': { }
+            'Edible Vegetation': { }
+            'Public Kitchens and Potlucks': { }
+            'Community Gardens': { }
+        </ul>
+    ': { }
+    'Supplies and Tools': { }
+    'Healthcare': { }
+    'Weather, Climate, & Geology
+        <ul>
+            'USGS Earthquakes': { }
+        </ul>
+    ': { }
+    'Pollution
+        <ul>
+            'Nuclear Facilities<br/>
+                <input type="range"  min="0" max="100" />
+            ': { }
+            'Superfund Sites': { }
+        </ul>
+    ': { }
+    'Society & Community': { }
+    'Transportation': { }
+    'Communication': { }
+    'Money': { }
+    'Entertainment': { }
+    'Freedom': { }
 
 
-                            <li>Shelter
-                                <ul>
-                                    <li>Bridges</li>
-                                    <li>Homeless Shelters</li>
-                                    <li>Hotels</li>
-                                    <li>Campgrounds</li>
-                                    <li>CouchSurfing</li>
-                                    <li>Hostels</li>
-                                    <li>Apartments and Sublets</li>
-                                    <li>Real Estate</li>
-                                    <li>Occupy</li>
-                                </ul>
-                            </li>
-                            <li>Food
-                                <ul>
-                                    <li>Groceries<br/>
-                                        <input type="range"  min="0" max="100" />
-                                    </li>
-                                    <li>Edible Vegetation</li>
-                                    <li>Public Kitchens and Potlucks</li>
-                                    <li>Community Gardens</li>
-                                </ul>
-                            </li>
-                            <li>Supplies and Tools</li>
-                            <li>Healthcare</li>
-                            <li>Weather, Climate, & Geology
-                                <ul>
-                                    <li>USGS Earthquakes</li>
-                                </ul>
-                            </li>
-                            <li>Pollution
-                                <ul>
-                                    <li>Nuclear Facilities<br/>
-                                        <input type="range"  min="0" max="100" />
-                                    </li>
-                                    <li>Superfund Sites</li>
-                                </ul>
-                            </li>
-                            <li>Society & Community</li>
-                            <li>Transportation</li>
-                            <li>Communication</li>
-                            <li>Money</li>
-                            <li>Entertainment</li>
-                            <li>Freedom</li>
-
-
-};
-
+*/
