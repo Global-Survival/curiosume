ec2321c864914ef7553653a13ee0f6468e40ea85
SeH <seh999@gmail.com>
2012-11-01 14:35:18 -0400

    font-size via scrollwheel working for multi cortexits

--- client/cortexit.js
+++ client/cortexit.js
@@ -4,18 +4,69 @@ function cortexitSetFrame(e, n) {
 	var p = $('#' + e[0].id + ' p')[n];
 	$('#' + e[0].id + ' .cortexitContent').html(p.innerHTML);
 	
+	//TODO store current frame in element data
+	
 }
 
-function cortexit(es) {
-	var e = $(es);
+function updateFont(c, deltaFontSize) {
+	var minFontSize = 6;
+	
+    if (c == null)
+        return;
+    
+    var oldSize = c.style.fontSize;
+    //assumes that fontSize is already specified
+    if (oldSize == undefined) {
+    	oldSize = '12px';
+    }
+    oldSize = oldSize.substring(0, oldSize.length-2);
+    oldSize = parseFloat(oldSize);
+    var fontSize = parseFloat(oldSize) + deltaFontSize;
+    if (fontSize < minFontSize) fontSize = minFontSize;
+    
+    c.style.fontSize = fontSize + "px"; 
+    
+    //TODO iterate and apply to child elements
+    
+//    var e = c.getElementsByTagName("a");
+//    for (var i = 0; i < e.length; i++) {
+//        e[i].style.fontSize = c.style.fontSize;
+//    }        
+        
+}
+
+function cortexit(elementID) {
+	var defaultFontSize = 16;
+	var fontSizeDelta = 4;
+	
+	var e = $('#' + elementID);
 	e.addClass('cortexitWrap');
 	
-	var internalP = $(es + ' p');
+	var internalP = $('#' + elementID + ' p');
 	internalP.addClass('pHidden');
 	
-	$('<div class="cortexitContent"></div>').appendTo(e);
-	$('<div class="cortexitMenu"><span class="fontSizer">v^</span><span class="prevFrame">&lt;-</span><span class="cortexitMenuButton">*/*</span><span class="prevFrame">-&gt;</span></div>').appendTo(e);
-
+	var content = $('<div class="cortexitContent" style="font-size: ' + defaultFontSize + 'px"></div>');
+	content.appendTo(e);
+	
+	var m = $('<div class="cortexitMenu"></div>');
+	
+	var fontSizer = $('<span class="fontSizer">v^</span>');
+	fontSizer.bind('mousewheel DOMMouseScroll', function(e, delta) {
+		delta = delta || event.detail || event.wheelDelta;
+		if (delta < 0)
+			updateFont(content[0], -fontSizeDelta);
+		else if (delta > 0)
+			updateFont(content[0], fontSizeDelta);			
+	});
+	
+	fontSizer.appendTo(m);
+	
+	$('<span class="prevFrame">&lt;-</span>').appendTo(m);
+	$('<span class="cortexitMenuButton">*/*</span>').appendTo(m);
+	$('<span class="prevFrame">-&gt;</span>').appendTo(m);
+	
+	m.appendTo(e);
+	
 	e.each(function () {
 	    var s = $(this);
 	    cortexitSetFrame(s, 0);
