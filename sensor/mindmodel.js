324f44c70e14c3dc342ef1f79d1dac39268db41c
SeH <seh999@gmail.com>
2012-11-11 18:17:25 -0500

    including chronozoom

--- sensor/mindmodel.js
+++ sensor/mindmodel.js
@@ -4,37 +4,57 @@
 var csv = require('ya-csv');
 var sensor = require('./sensor.js');
 
+var readCSV = function(path, out, finished) {
+	var reader = csv.createCsvFileReader(path, {
+	    'separator': '\t'
+//	    'quote': '"',
+//	    'escape': '"',       
+//	    'comment': '',
+	});
+
+	reader.addListener('data', function(data) {
+		out(getEntry(data));
+	});
+
+	if (finished) {
+		reader.addListener('end', function() {
+			finished();
+		});
+	}
+}
+
+var getEntry = function(data) {
+
+	function spaceToUnderscore(x) {
+		if (x == undefined)
+			return "_";
+		return x.replace(/ /g, '_');
+	}
+
+	data[0] = spaceToUnderscore(data[0]);
+	data[1] = spaceToUnderscore(data[1]);
+	data[2] = spaceToUnderscore(data[2]);
+	data[3] = spaceToUnderscore(data[3]);
+	data[4] = spaceToUnderscore(data[4]);
+
+	var srcID = data[0] + '.' + data[1];
+	var targetID = data[3] + '.' + data[4];
+
+	var o = {
+		id: srcID,
+		type: data[0],
+		statement: [
+			[ data[2], targetID ]
+		]
+	};
+
+	return o;
+}
+
 var MMCSV = function(path) {
 	var s = sensor.Sensor('MMSCV_' + path, function() {
 
-		var reader = csv.createCsvFileReader(path, {
-		    'separator': ',',
-		    'quote': '"',
-		    'escape': '"',       
-		    'comment': '',
-		});
-
-		reader.addListener('data', function(data) {
-			function spaceToUnderscore(x) {
-				return x.replace(/ /g, '_');
-			}
-			data[0] = spaceToUnderscore(data[0]);
-			data[1] = spaceToUnderscore(data[1]);
-			data[2] = spaceToUnderscore(data[2]);
-			data[3] = spaceToUnderscore(data[3]);
-			data[4] = spaceToUnderscore(data[4]);
-
-			var srcID = data[0] + '.' + data[1];
-			var targetID = data[3] + '.' + data[4];
-
-			var o = {
-				id: srcID,
-				type: data[0],
-				statement: [
-					[ data[2], targetID ]
-				]
-			};
-	
+		readCSV(path, function(o) {		
 			s.out.push(o);
 		});
 
@@ -43,4 +63,6 @@ var MMCSV = function(path) {
 	return s;
 }
 
+exports.readCSV = readCSV;
+exports.getEntry = getEntry;
 exports.MMCSV = MMCSV;
