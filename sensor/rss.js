27de677b3cad4fa8f78a313e02f3d674f8382b37
SeH <seh999@gmail.com>
2012-11-13 01:49:51 -0500

    rss feed reader with georss support

--- sensor/rss.js
+++ sensor/rss.js
@@ -1,1 +1,35 @@
+//https://github.com/danmactough/node-feedparser
+
+var feedparser = require('feedparser');
+var sensor = require('./sensor.js');
+
+var RSSFeed = function(channel, url) {
+	var s = sensor.Sensor('RSS_' + url, function() {
+
+		function onArticle(a) {
+			//console.dir(a);
+			
+			var x = {
+				uuid: a['guid'],
+				link: a['link'],
+				when: new Date(a['date']).getTime(),
+				name: a['title'],
+			};
+			if (a['georss:point']) {
+				x.geolocation = a['georss:point'];
+			}
+
+			//console.dir(x);
+			s.out.push([channel, x]);
+		}
+
+		feedparser.parseUrl(url)
+  			.on('article', onArticle);
+
+	}, function() { 	});
+
+	return s;
+}
+
+exports.RSSFeed = RSSFeed;
 
--- server/web.js
+++ server/web.js
@@ -450,7 +450,8 @@ var b = util.OutputBuffer(500, function(o) {
 b.start();
 
 sensor.setDefaultBuffer(b); 
-sensor.addSensor(require('../sensor/googlefinance.js').GoogleFinanceSymbols(['aapl','msft','ibm','goog']));
+//sensor.addSensor(require('../sensor/googlefinance.js').GoogleFinanceSymbols(['aapl','msft','ibm','goog']));
+sensor.addSensor(require('../sensor/rss.js').RSSFeed('x', 'http://blog.automenta.com/feeds/posts/default?alt=rss'));
 //sensor.addSensor(require('../sensor/mindmodel.js').MMCSV('emotion-happy','./schema/enformable_atomic_history.statements.tsv'));
 //sensor.addSensor(require('../sensor/echo.js').Echo('emotion-happy', 'happiness'));
 //sensor.addSensor(require('../sensor/echo.js').Echo('emotion-surprised', 'surprise!'));
