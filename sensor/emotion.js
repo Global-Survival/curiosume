37f171ce20fe6fcec89e37c0cb8e7efdc8ab2699
SeH <seh999@gmail.com>
2012-12-12 14:53:26 -0500

    updates

--- sensor/emotion.js
+++ sensor/emotion.js
@@ -2,13 +2,36 @@ var sensor = require('./sensor.js');
 
 var emotionTypes = [
 	{ uri: 'emotion.happy', name: 'Happy' },
+	{ uri: 'emotion.trusting', name:'Trusting'},
+	{ uri: 'emotion.anticipating', name: 'Anticipating'},
+	{ uri: 'emotion.surprised', name:'Surprised'}, 
+	
+    //[abstract NEGATIVE supertype]
+	/*
+	"NEGATIVE EVENT" CBT Questions that Can Be Stored as Properties
+	 
+	Help decide whether the meanings you give to a negative event cause disturbance by answering:
+
+		Is the meaning I’m assigning to this event unduly extreme? 
+		Am I exaggerating a simple event to derive harsh conclusions from it?
+		Am I drawing global conclusions from this isolated event? 
+		Am I deciding that this 1 event totally defines me? 
+		Am I deciding that this specific situation affects my future?
+		
+		Does this meaning lead me to feel better or worse about myself? 
+		Is it encouraging further goal-directed action or discouraging me to give up?
+
+	If answers to these questions are largely ‘yes’, 
+	then you probably are disturbing yourself unnecessarily about a negative event. 
+
+	The situation may in fact be negative – but your thinking may be worsening it.
+	*/	
 	{ uri: 'emotion.afraid', name: 'Afraid'},
 	{ uri: 'emotion.sad', name: 'Sad'},
 	{ uri: 'emotion.angry', name: 'Angry'},
-	{ uri: 'emotion.disgusted', name: 'Disgusted'},
-	{ uri: 'emotion.trusting', name:'Trusting'},
-	{ uri: 'emotion.anticipating', name: 'Anticipating'},
-	{ uri: 'emotion.surprised', name:'Surprised'} 
+	{ uri: 'emotion.disgusted', name: 'Disgusted'}
+	
 ];
+
 
 sensor.addTypes(emotionTypes);
---
+++ sensor/enterprise.js
@@ -0,0 +1,89 @@
+//FROM: http://equitylancer.com/search
+
+/*
+	Compensation
+  		Position: Full-time, Part-time, Independent Contractor
+  		Cash Salary
+  		Equity Salary
+  		Deferred Cash + Equity Salary
+
+ 	Experience Level:
+		2 - 5 years 
+		6-10 years 
+		11 or more years 
+		Entry Level 
+		Internship/Co-op
+*/
+/*
+CATEGORIES
+----------
+Accounting/Finance 
+Advertising/Public Relations 
+Advisor 
+Arts/Entertainment/Publishing 
+Automotive 
+Banking/Mortgage 
+Biotech 
+Board Member 
+Broadcast 
+Business Development 
+Clerical/Administrative 
+Construction/Facilities 
+Consultant 
+Customer Service 
+Design 
+Distribution 
+Education 
+Engineering 
+Entry Level 
+Executive 
+Facilities 
+Food Service 
+General Business 
+General Labor 
+Government 
+Grocery 
+Healthcare 
+Hospitality 
+Hotel 
+Human Resources 
+Information Technology 
+Installation 
+Insurance 
+Inventory 
+Journalism 
+Legal 
+Legal Admin 
+Maintenance 
+Management 
+Manufacturing 
+Marketing 
+Mobile 
+New Grad 
+Nurse 
+Other 
+Pharmaceutical 
+Planning 
+Procurement 
+Professional Services 
+Purchasing 
+QA 
+Quality Control 
+Repair 
+Research 
+Restaurant 
+Retail 
+Sales 
+Science 
+Shipping 
+Skilled Labor 
+Software Development 
+Strategy 
+Supply Chain 
+Telecommunications 
+Trades 
+Training 
+Transportation 
+Warehouse 
+Web Development
+*/
