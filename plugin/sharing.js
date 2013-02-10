8fc2722934e0693c95a6659aae3ddf5dfbe30653
SeH <seh999@gmail.com>
2013-02-09 19:56:59 -0500

    updates

---
+++ plugin/sharing.js
@@ -0,0 +1,131 @@
+/** 
+Collaborative Consumption services
+	Originally from a diagram by Rachel Botsman @ http://CollaborativeConsumption.com
+	Transcribed to text by Dante Monson
+*/
+
+/*
+Redistribution :
+
+- Big Market Places
+( craigslist , ebay , gumtree )
+
+- Free / Gift Exchanges
+( freecycle , giftflow , freally , exchango )
+
+- Swap sites for books
+( paperbackswap , bookmooch )
+
+- Swapsites for baby goods and toys
+( toyswap , thredyp , swapitbaby , tauschteddy )
+
+- Clothing Swaps
+( swapstyme , the clothing exchange , 99 dresses , poshmark  )
+
+- Swap sites for media ( dvd's , books , games )
+( swap , dig n'swap , netcycler , swapsity )
+
+- Neighborhood Marketplaces
+( zaarly , garage sale trail )
+
+Assets and Services :
+
+- Car sharing
+( zipcar , goget , whizzcar , autoshare , stattauto , autolibre )
+
+- Car Sharing ( big automobile brands )
+( bwn drivenow , volkswagen quicqr , Peugeot MU , Daimler Car2Go )
+
+- Peer to Peer Car Sharing
+( Whipcar , relayrised , drivemy car rentals , getarand , tamyca )
+
+- Bike Sharing
+( Velib , Bixi , Barclays  Cycle Hire , B-Cycle , Call-A-Bike , Cyclocity )
+
+- Ride Sharing
+( Zimride , Nuride , Liftshare , Jayride , Gocarshare , Carpooling )
+
+- Solarpower
+( Solarcity , Solar Century , Pretasol , One Block Off the Grid )
+
+- Toy Rental
+( Babyplays , Rent-a-Toy )
+
+- Textbook Rental
+( Chegg , Campusbookrentals , Zookal , Bookrenter ) 
+
+- Art Rental
+( Art.sy , Artiscle , Turningart )
+
+- Fashionrental
+( Bag Borrow or Steal , Fashion Hier , Dress Vault , Rent the Runway )
+
+- Movies
+( Netflix , Quickflix , Lovefilm )
+
+- General Online Rental
+( Getable , Anyhire , Snapgoods , Neighborgoods , Dogvacay )
+
+- Peer-to-Peer Rental
+( Zilok , Rentoid , Ecomodo , Hire Things , Rentstuff )
+
+- Neighborhood Rental
+( Share Some Sugar , Neighbor Row , The Sharehood , Frents )
+
+
+ Collaborative Lifestyles
+
+- Storage Networks
+( Storepod , Sharemystorage , Spaceout
+
+- Social Food Networks
+( Gobble , Grubwithus , Eatwithme , wok+wine )
+
+- Peer-to-Peer Car Sharing
+( Tamyca , Buzzcar , Nachbarschaftsauto , autonetzer , snappcar )
+
+- Errand and Task Networks
+( Zaarly , Airrun , Mytaskanger , Gigwalk , Airtasker , Taskrunner )
+
+- Neighborhood Support
+( Neighborhoodgoods, Share some sugar , Streetbank , Toolzdo )
+
+- Parkingspots
+( Parkatmyhouse , Parkmcirca , Parkonmydrive )
+
+- Sharedstudios / Workshops
+( 3rd space studios , Techshop )
+
+- Skill Sharing
+( Brooklyn Skill Share , Tradeschool , Skillshare , Weteachme )
+
+- Gardens
+( Urbangardenshare , landshare , yardshare )
+
+
+- Crowdfunding
+( Startsomegood , Pozible , Crowdcube , Catarse )
+
+- Bartering
+( Ourgoods , Itex , Bartercard , Tourboarding )
+
+- Taxi Sharing
+( Taxi2 , Taxistop , Weeels )
+
+- Peer-to-Peer Travel
+( Airbnb , Roomoroama , One Fine Stay , Bed and Fed , 9 Flats )
+
+- Social Currencies
+( Ven , The Liquidity Network , Time Banks , Letsystems )
+
+- Social Lending
+( Zopa , Prosper , The Lending Club )
+
+- Co-working Space Finders
+( desknearme , deskwanted , opendesks , desksurfing )
+
+- Co-working Spaces
+( networkcity , studiomates , beesoffice , coloft )
+
+
+*/
