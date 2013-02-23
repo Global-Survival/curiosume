3f558abc77d0aea2e59ea74480f68a34ea9f6bbd
SeH <seh999@gmail.com>
2013-02-23 10:17:24 -0500

    defense updates

--- plugin/defense.js
+++ plugin/defense.js
@@ -1,7 +1,16 @@
 /*
 
-http://defcad.org/
 http://defensedistributed.com/
+	http://defcad.org
+
 http://en.wikipedia.org/wiki/Martial_arts
+	http://en.wikipedia.org/wiki/Krav_Maga
+	http://en.wikipedia.org/wiki/Jeet_Kune_Do
+
+http://en.wikipedia.org/wiki/First_Earth_Battalion
+	http://arcturus.org/field_manual.pdf - Field Manual
+
+http://en.wikipedia.org/wiki/United_States_Army_Field_Manuals
+	http://www.loc.gov/rr/frd/Military_Law/pamphlets_manuals.html
 
 */
