17665cd0af840975f7c28a08c1a3b3021bdf260e
SeH <seh999@gmail.com>
2013-02-26 17:38:02 -0500

    natural resources

---
+++ plugin/natural_resource.js
@@ -0,0 +1,80 @@
+/*
+Natural Resources
+    http://en.wikipedia.org/wiki/Natural_resource
+    http://sweet.jpl.nasa.gov/2.2/matrNaturalResource.owl
+    
+    Thanks for initial ideas contributed by Wolfgang Hoeschele
+    
+----
+
+Biotic vs. Abiotic, Renewable vs. Non-renewable
+
+Land
+
+Air
+
+Heavy metals 
+    including ores such as gold, iron, copper, silver, etc.
+
+Animals
+
+Plantlife and Fungi
+
+Fossil Fuels
+
+    Coal
+    
+    Petroleum
+
+Water (H2O) - patterns of abundance and scarcity of clean water
+
+        sources
+        
+            rain
+            
+            seas, oceans, lakes, rivers, streams, creeks, ponds, irrigation
+                        
+            underground water
+            
+            glaciers
+            
+            ice caps (and their melting)
+            
+            space (comets, meteors, asteroids, moons)
+        
+        applications
+        
+            drinking
+            
+            bathing
+        
+            crops
+    
+            plumbing / piped water
+            
+            industrial / factory applications
+            
+            ...
+    
+        potability (fresh / drinking water)
+      
+        salinity (salt water)
+        
+        precipitation
+        
+        evaporation
+        
+        surface and ground water availability
+        
+        capacity to desalinize sea water
+            (which I think could be greatly enhanced if solar energy was used for this purpose effectively)
+                
+        water quality & pollution
+            hydrofracking
+            fluoridation
+            ...
+    
+
+
+
+*/
