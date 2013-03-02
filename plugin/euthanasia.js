c0d106ded2c154079cd2e1b0c4f457dede5a9450
SeH <seh999@gmail.com>
2013-03-01 20:13:39 -0500

    updates

--- plugin/euthanasia.js
+++ plugin/euthanasia.js
@@ -1,34 +1,80 @@
 /*
-http://en.wikipedia.org/wiki/Euthanasia "good death" (for humans and animals)
+"Good Death" for humans, animals, and all natural & artificial beings
+
+http://en.wikipedia.org/wiki/Euthanasia 
 http://en.wikipedia.org/wiki/Animal_euthanasia
 http://en.wikipedia.org/wiki/Suicide
+http://en.wikipedia.org/wiki/Category:Death_customs
+http://en.wikipedia.org/wiki/Resurrection
+http://en.wikipedia.org/wiki/After_death
+    http://en.wikipedia.org/wiki/Bardo
+    http://en.wikipedia.org/wiki/Reincarnation
+        http://en.wikipedia.org/wiki/Reincarnation_research
+
 
 index of euthanasia methods, sortable by
 
-	efficacy
+    efficacy and medical consequences of failure
 
 	resources needed
+        chemicals and pharmaceuticals
+        herbs and botanicals
+        machines and devices
+        animal, human, or robotic assistants
 
 	safety of others
 
-	method of of body disposal
+	how method affects possible methods body disposal
 
-	level of pain
+	level and duration of pain
+    
+    places where euthanasia is legal
 
 
 euthanasia alternatives and remediation
 
+    hospitalization
+        induced coma
+        
+    psychotherapy
+    
 	pain killing
-
 		opiates
 			potentiators to reduce dependency and addiction and increase efficiency
 
 		dissociatives
 		anaesthetics
 
-	end of life anxiety
-		
+	end of life anxiety		
 		http://www.maps.org/research/psilo-lsd/
 		
+    cryonics
+        http://en.wikipedia.org/wiki/Cryonics
+
+
+last will
+    http://en.wikipedia.org/wiki/Will_(law)
+
+
+burial
+    http://en.wikipedia.org/wiki/Burial
+    http://en.wikipedia.org/wiki/Funeral
+
+
+mourning for deceased
+    http://en.wikipedia.org/wiki/Mourning
+
+
+autopsy
+    http://en.wikipedia.org/wiki/Autopsy
+    
+    reasons and methods
+
+
+lifelog avatar resurrection
+    http://en.wikipedia.org/wiki/Lifelogging
+    
+    
+rebirth determination
 
 */
