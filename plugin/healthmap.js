6b2e25768a96ad0d311f7a692422fb0a552a0f32
SeH <seh999@gmail.com>
2013-03-24 14:39:03 -0400

    healthmap.org notes

---
+++ plugin/healthmap.js
@@ -0,0 +1,16 @@
+/*
+http://www.healthmap.org/en/
+http://www.healthmap.org/hm.server.new.php
+
+HTTP POST
+xajax:buildNowShowing
+xajaxr:1364150197180
+xajaxargs[]:
+xajaxargs[]:
+xajaxargs[]:
+xajaxargs[]:true
+xajaxargs[]:
+xajaxargs[]:ns_54bf7042856317e95f4da919ab665a1b
+xajaxargs[]:
+*/
+
