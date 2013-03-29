91d4dca84c6a85f216dbfcc6f90731ac1f1adaf4
SeH <seh999@gmail.com>
2013-03-29 16:12:08 -0400

    EDIS almost completely parsed

--- plugin/global_incident.js.js
+++ plugin/global_incident.js.js
@@ -13,16 +13,6 @@ http://cc.rsoe.hu/rss/rss.php
     </gml:Point>
     </georss:where>
 
-hisz.rsoe.hu/alertmap
-
-	<link rel="alternate" type="application/rss+xml" title="RSOE EDIS - Realtime Disaster and Emergency Information in the World" href="http://feeds.feedburner.com/RsoeEdis-EmergencyAndDisasterInformation"/>
-	<link rel="alternate" type="application/rss+xml" title="RSOE EDIS - Airplane Accident and Incident Monitor (USA/FAA)" href="http://feeds.feedburner.com/rsoeEdis-FaaAirplaneAccidentAndIncidentInformation"/>
-	<link rel="alternate" type="application/rss+xml" title="RSOE EDIS - Epidemic and Biology Hazard Warning Message" href="http://feeds.feedburner.com/RsoeEdis-Ebhwm"/>
-	<link rel="alternate" type="application/rss+xml" title="RSOE EDIS - Global Tropical Storm Monitoring" href="http://feeds.feedburner.com/RsoeEdis-TropicalStormInformations"/>
-	<link rel="alternate" type="application/rss+xml" title="RSOE EDIS - UV Index in the USA" href="http://feeds.feedburner.com/RsoeEdis-UvIndexInTheUsa"/>
-	<link rel="alternate" type="application/rss+xml" title="RSOE EDIS - Global Volcano Status" href="http://feeds.feedburner.com/RsoeEdis-VolcanoMonitoring"/>
-	<link rel="alternate" type="application/rss+xml" title="RSOE EDIS - Preliminary Earthquake Report" href="http://feeds2.feedburner.com/RsoeEdis-EarthquakeReportM25"/>
-	<link rel="alternate" type="application/rss+xml" title="RSOE EDIS - Tsunami Information" href="http://feeds2.feedburner.com/RsoeEdis-TsunamiInformation"/>
 
 www.globalincidentmap.com
     markers.push({'latitude':33.511100,'longitude':36.306400,'eventtype':'General Terrorism News','country':'Syria','city':'Damascus','imagefilepath':'mapicons/general.gif'});
@@ -34,11 +24,11 @@ var _ = require('underscore');
 
 
 exports.plugin = {
-        name: 'Disasters',	
+        name: 'Global Incident Map',	
 		description: '',
 		options: { },
         version: '1.0',
-        author: 'http://netention.org',
+        author: 'http://www.globalincidentmap.com',
         
 		start: function(netention, util) { 
             http.get("http://www.globalincidentmap.com", function(res) {
@@ -74,7 +64,7 @@ exports.plugin = {
                 });  
               
             }).on('error', function(e) {
-              console.log("Disaster loading Error: " + e.message);
+              console.log("Global Incident loading Error: " + e.message);
             });
             
         },
